9a4a8fac83fb4d4cf0255fec6f066e8b533d7ad9
titov <vladimir.titov@yumalabs.ru>
2017-05-12 17:02:11 +0300

    add images, seed database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512131239) do
+ActiveRecord::Schema.define(version: 20170512134402) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
