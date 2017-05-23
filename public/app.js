7e4914d85a11ad0be97af412c2134ab5f40d3b49
nafan <nafanpzk@mail.ru>
2017-05-23 22:16:20 +0300

    add function total_count for items in cart

--- public/app.js
+++ public/app.js
@@ -16,4 +16,20 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	alert('Items in your cart: ' + total_count());
+}
+
+function total_count()
+{
+	var cnt = 0;
+
+	for (var i = 0; i < window.localStorage.length; i++) {
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		cnt = cnt + value * 1;
+	}
+
+	return cnt;
 }
