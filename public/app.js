961bb06f00309f23c49d042ca023df7507ef8dcf
nafan <nafanpzk@mail.ru>
2017-05-23 20:29:59 +0300

    add hash for save products in cart

--- public/app.js
+++ public/app.js
@@ -11,5 +11,7 @@ function something()
 
 function add_to_cart(id)
 {
-	alert('You added to card pizza with id ' + id);
+	var x = window.localStorage.getItem('product_' + id);
+	x = x * 1 + 1;
+	window.localStorage.setItem('product_' + id, x);
 }
