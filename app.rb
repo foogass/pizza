6159c1c348a661d7003d9ad1824b8c931fce6a00
titov <vladimir.titov@yumalabs.ru>
2017-06-09 13:50:40 +0300

    parse orders input

--- app.rb
+++ app.rb
@@ -20,5 +20,29 @@ get '/about' do
 end
 
 post '/cart' do
-	erb 'hello'
+	orders_input = params[:orders]
+	@orders = pars_orders_line(orders_input)
+ 	erb "hello #{@orders.inspect}"
+end
+
+def pars_orders_line orders_input
+
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split(/\=/)
+
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+		
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+
+	return arr
+
 end
